0d5df12d30a2d1ff7ccd8a9680cdf39afd9b6d1a
Evgen904 <evgen904@gmail.com>
2019-12-18 20:29:26 +0300

    fix

--- app/scripts/src/_includes/main.js
+++ app/scripts/src/_includes/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('.btn').on('click',function(event){
+    $('.btn-new').on('click',function(event){
         event.preventDefault();
         var popupClick = $('[data-remodal-id=popupClick]').remodal();
             popupClick.open();
